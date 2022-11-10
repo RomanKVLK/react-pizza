e92b4519cc56f44d6db2627d8f14637564180727
RomanKVLK <romnik20021994@gmail.com>
2022-11-10 13:48:21 +0300

    Search, pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,22 +4,30 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock/index';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
+import { SearchContext } from '../App';
 
 const Home = () => {
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: 'популярности',
     sortProperty: 'rating',
   });
+  const { searchValue } = React.useContext(SearchContext);
+
+  const items = pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
   React.useEffect(() => {
     setIsLoading(true);
+    const search = searchValue ? `&search=${searchValue}` : '';
     fetch(
-      `https://635eb78303d2d4d47af4dab0.mockapi.io/pizzas?${
+      `https://635eb78303d2d4d47af4dab0.mockapi.io/pizzas?limit=8&page=${currentPage}&${
         categoryId > 0 ? `category=${categoryId}` : ''
-      }&sortBy=${sortType.sortProperty}&order=desc`,
+      }&sortBy=${sortType.sortProperty}&order=desc${search}`,
     )
       .then((res) => {
         return res.json();
@@ -29,7 +37,7 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
   return (
     <>
       <div className="content__top">
@@ -37,13 +45,17 @@ const Home = () => {
         <Sort value={sortType} onChangeType={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : items}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </>
   );
 };
 
 export default Home;
+
+// .filter((obj) => {
+//   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+//     return true;
+//   }
+//   return false;
+// })
